68487c0bc528bd3fa26d8623aa8369314352790e
ritesh vyawahare <riteshv919@gmail.com>
2024-12-07 01:47:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+const express = require("express");
+
+const app = express();
+
+
+const mongoose = require('mongoose');
+const path = require("path");
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+}
+
+
+app.get("/", (req, res) => {
+
+    res.send("root is working")
+
+})
+app.listen(8080, () => {
+    console.log("working")
+});
---
+++ models/chat.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+
+const chatSchema = new mongoose.Schema({
+    fr
+})
